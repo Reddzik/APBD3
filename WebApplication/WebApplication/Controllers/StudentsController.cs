53c04c3f66ccad3aba6ba31e3e41b3e3692eac72
Reddzik <mateusz.redzik@o2.pl>
2020-03-19 13:56:20 +0100

    +delete put

--- WebApplication/WebApplication/Controllers/StudentsController.cs
+++ WebApplication/WebApplication/Controllers/StudentsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
@@ -28,6 +29,26 @@ namespace WebApplication.Controllers
             }
 
             return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            //jeśli istnieje
+            return Ok($"Usunąłem studenta o id = {id}");
+
+        }
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(string oldValue, int id, string newValue)
+        {
+            return Ok($"zmieniam wartość {oldValue} usera o id= {id} na wartość {newValue} podaną w query stringu");
+
         }
 
     }
